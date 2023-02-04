357bb384d96f4ce96e2cfc8423fbbdfb8c575dc9
mvictoria <micah.victoria@gmail.com>
2023-02-03 19:05:53 -0700

    format with black

--- rss_feed_reader/feed_reader.py
+++ rss_feed_reader/feed_reader.py
@@ -1,19 +1,21 @@
 import feedparser
 import click
 
+
 def read(url: str):
     feed = feedparser.parse(url)
     return feed.feed
 
 
 @click.command()
-@click.argument('url')
+@click.argument("url")
 def main(url):
     feed = read(url)
 
     click.echo(f"Title: {feed.title}")
     click.echo(f"Description: {feed.description}")
     click.echo(f"Link: {feed.link}")
+
 
 if __name__ == "__main__":
     main()
