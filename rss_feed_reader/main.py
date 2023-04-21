777b46936fe2cf2132ea7c0f93ee75cf51256ba0
Micah Victoria <micah.victoria@gmail.com>
2023-04-20 21:53:39 -0400

    create simple web form and get/post methods

--- rss_feed_reader/main.py
+++ rss_feed_reader/main.py
@@ -1,12 +1,14 @@
 import feedparser
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, Request
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 def root():
-    return {"Hello": "world"}
+    return "Hello there"
 
 
 @app.get("/read/{url:path}")
@@ -18,3 +20,26 @@ def read_rss_feed(url: str):
         "Description": feed.get("description", "No description"),
         "Link": feed.get("link", "No link"),
     }
+
+
+@app.get("/reader")
+def form_get(request: Request):
+    result = "Enter an RSS Feed "
+    return templates.TemplateResponse(
+        "form.html", context={"request": request, "result": result}
+    )
+
+
+@app.post("/reader")
+def form_post(request: Request, num: str = Form(...)):
+    feed = feedparser.parse(num).feed
+
+    return templates.TemplateResponse(
+        "form.html",
+        context={
+            "request": request,
+            "title": feed.get("title", "No title"),
+            "link": feed.get("link", "No link"),
+            "description": feed.get("description", "No description"),
+        },
+    )
